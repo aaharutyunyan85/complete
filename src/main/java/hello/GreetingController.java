db82c8055266502d97aa45792558cb9186e3278d
DD <you@example.com>
2018-01-13 21:13:54 +0400

    initial

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value="/register", method= RequestMethod.GET)
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
